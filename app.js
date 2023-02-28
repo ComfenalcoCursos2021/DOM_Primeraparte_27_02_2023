a6a3764aca318042019abd3d92c46a1194781aa2
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2023-02-27 20:48:13 -0500

    Selecion del DOM y modularizacion

---
+++ app.js
@@ -0,0 +1,38 @@
+import main from './js/main.js'
+
+
+main.setValue("Angel Castro Escamilla");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Selecion de etiquetas con EMC6
+
+// console.log(document.querySelector("#nombre"));
+// console.log(document.querySelectorAll(".NOMBRE"));
+// console.log(document.querySelector(`[name="NomBre"]`));
+
+
+
+
+
+
+
+
+// Selecion de etiquetas con javaScript
+
+// console.log(document.getElementById("nombre"));
+// console.log(document.getElementsByTagName("div").nombre);
+// console.log(document.getElementsByClassName("NOMBRE")[0]);
+// console.log(document.getElementsByName("NomBre")[0]);
